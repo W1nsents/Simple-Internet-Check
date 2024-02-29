bdafb206bcd1b4cb291aab245bea430050413410
Дмитрий <99503115+W1nsents@users.noreply.github.com>
2024-02-29 13:10:28 +0400

    Add files via upload

---
+++ Simple.Internet.Checker/Internet.Checker/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internet.Checker
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Internet.OK())
+            {
+                Console.WriteLine("Connect");
+            }
+            else
+            {
+                Console.WriteLine("Сonnection lost");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Internet
+    {
+        public static bool OK()
+        {
+            try
+            {
+                Dns.GetHostEntry("google.com");
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
